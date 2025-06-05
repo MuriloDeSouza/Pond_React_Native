d593e15221257e50c0a9566e3b0b96b60ec9bba1
Murilo Prianti <murilo.silva@sou.inteli.edu.br>
2025-06-05 10:53:02 -0300

    ajuste nos caminhos das telas

--- pond_react_native/App.js
+++ pond_react_native/App.js
@@ -1,3 +1,69 @@
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, View } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// // Importações das telas
+// import Login from './telas/Login';
+// import Cadastro from './telas/Cadastro';
+// // import RecuperarSenha from './telas/RecuperarSenha';
+// // import Home from './telas/Home';
+// // import Perfil from './telas/Perfil';
+// // import ProductDetail from './telas/ProductDetail';
+
+// const Stack = createNativeStackNavigator();
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <NavigationContainer>
+//         <StatusBar style="auto" />
+//         <Stack.Navigator
+//           initialRouteName="Login"
+//           screenOptions={{
+//             headerShown: false,
+//             animation: 'slide_from_right', // Animação suave entre telas
+//           }}
+//         >
+//           {/* Telas de autenticação */}
+//           <Stack.Screen name="Login" component={Login} />
+//           <Stack.Screen name="Cadastro" component={Cadastro} />
+//           {/* <Stack.Screen name="RecuperarSenha" component={RecuperarSenha} /> */}
+
+//           {/* Telas principais do app */}
+//           {/* <Stack.Screen 
+//             name="Home" 
+//             component={Home} 
+//             options={{ 
+//               gestureEnabled: false // Impede voltar para o Login com gesto
+//             }}
+//           /> */}
+          
+//           {/* <Stack.Screen name="Perfil" component={Perfil} />
+          
+//           <Stack.Screen 
+//             name="ProductDetail" 
+//             component={ProductDetail}
+//             options={{ 
+//               title: 'Detalhes do Produto',
+//               headerShown: true, // Mostra header apenas nesta tela
+//               headerBackTitle: 'Voltar' // Texto do botão de voltar (iOS)
+//             }}
+//           /> */}
+//         </Stack.Navigator>
+//       </NavigationContainer>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//   },
+// });
+
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
